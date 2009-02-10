1739abf7e583ab88ab9bd52fd461a9a702ff781d
Gabriel Horner <gabriel.horner@gmail.com>
2009-02-10 05:03:13 -0500

    moved out machine tags to its own gem

--- vendor/plugins/has_machine_tags/init.rb
+++
@@ -1,3 +0,0 @@
-require 'has_machine_tags'
-
-ActiveRecord::Base.send :include, HasMachineTags::ActiveRecord
--- vendor/plugins/has_machine_tags/lib/has_machine_tags.rb
+++
@@ -1,7 +0,0 @@
-current_dir = File.dirname(__FILE__)
-$:.unshift(current_dir) unless $:.include?(current_dir) || $:.include?(File.expand_path(current_dir))
-
-module HasMachineTags
-  module ActiveRecord
-  end
-end
