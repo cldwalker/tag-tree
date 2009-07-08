e69d9f808f8fdf864fa38fab5b08c5e26d1947e7
Gabriel Horner <gabriel.horner@gmail.com>
2009-07-08 05:22:06 -0400

    fixed ol method_missing

--- lib/irb/core_extensions.rb
+++ lib/irb/core_extensions.rb
@@ -1,14 +1,17 @@
-class Array
+module MyArray
   def amap(*fields)
     map {|e| fields.map {|field| e.send(field) }}
   end
+
   def method_missing(method,*args,&block)
-          shortcut_klasses = [ActiveRecord::Base]
-          #if all have one of shortcut klasses as an ancestor
-          if self.all? {|e| shortcut_klasses.any? {|k| e.is_a?(k)} }
-            self.map {|e| e.send(method,*args,&block) }
-          else
-            super
-          end
+    shortcut_klasses = [ActiveRecord::Base]
+    #if all have one of shortcut klasses as an ancestor
+    if !self.empty? && self.all? {|e| shortcut_klasses.any? {|k| e.is_a?(k)} }
+      self.map {|e| e.send(method,*args,&block) }
+    else
+      super
+    end
   end
 end
+
+Array.send :include, MyArray
