ab5cbb7e1c7612b4daf7cef44e712cbda363ab13
Gabriel Horner <gabriel.horner@gmail.com>
2009-09-06 07:48:44 -0400

    bye iam it's been fun

--- config/environment.rb
+++ config/environment.rb
@@ -66,4 +66,3 @@ Rails::Initializer.run do |config|
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
 end
-load "lib/irb.rb" if $0 == 'irb'
--- lib/irb.rb
+++
@@ -1,3 +0,0 @@
-# require 'irb/active_record_extensions'
-# require 'irb/core_extensions'
-# require 'irb/console_shortcuts'
