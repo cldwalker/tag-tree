e9949ef5a58f95fa1aafafd341a6398b8a614a7b
Gabriel Horner <gabriel.horner@gmail.com>
2010-01-13 17:40:32 -0500

    refactored default predicates

--- lib/default_predicate.rb
+++ lib/default_predicate.rb
@@ -1,5 +1,13 @@
 # Determines default predicates for machine tagged content based on rules in config/machine_tags.yml
 class DefaultPredicate
+  CONFIG_FILE = RAILS_ROOT + '/config/machine_tags.yml'
+
+  attr_accessor :rule, :global, :filter, :predicate
+  def initialize(rule)
+    @rule = rule
+    @global = !include?(Tag::PREDICATE_DELIMITER)
+  end
+
   class <<self
     # Used by Url to find default predicate
     def find(value, namespace)
@@ -7,39 +15,57 @@ class DefaultPredicate
       (match = default_predicates.find {|e| mtag_to_match[/#{e[0].gsub('*', '.*')}/]}) ? match[1] : 'tags'
     end
 
-    # default predicate methods
-    def machine_tag_config(reload=false)
-      @config = reload || @config.nil? ? YAML::load_file(RAILS_ROOT + '/config/machine_tags.yml') : @config
+    # :static_predicates: ?
+    # - "*:*=(ruby|perl|sh|python|js|flash|cpp|bash)"
+    # - plang
+    def config
+      @config ||= read_config
     end
 
-    def global_predicates(reload=false)
-      if reload || @global_predicates.nil?
-        @global_predicates = machine_tag_config[:global_predicates].map {|e| 
-          mtags = Tag.machine_tags(Tag.build_machine_tag('*', e, '*')); ["*:*=(#{mtags.map(&:value).uniq.join('|')})", e]}
-      end
-      @global_predicates
+    def read_config
+      File.exists?(CONFIG_FILE) ? YAML::load_file(CONFIG_FILE) : {:global_predicates=>[], :dynamic_predicates=>[]}
     end
 
-    def wildcard_predicates(reload=false)
-      if reload || @wildcard_predicates.nil?
-        @wildcard_predicates = machine_tag_config[:dynamic_predicates].map {|e| 
-          mtags = Tag.machine_tags(e); ["#{mtags[0].namespace}:*=(#{mtags.map(&:value).uniq.join('|')})", mtags[0].predicate]}
-      end
-      @wildcard_predicates
+    def add_rules(*rules)
+      new_config = config.dup
+      rules.each {|e|
+        e.include?(Tag::PREDICATE_DELIMITER) ? new_config[:dynamic_predicates] << e :
+          new_config[:global_predicates] << e
+      }
+      File.open(CONFIG_FILE, 'w') {|f| f.write(new_config.to_yaml) }
+      new_config
     end
-    
+
+    def global_predicates
+      @global_predicates ||= generate_global_predicates
+    end
+
+    def generate_global_predicates
+      config[:global_predicates].map {|e|
+          mtags = Tag.machine_tags(Tag.build_machine_tag('*', e, '*')); ["*:*=(#{mtags.map(&:value).uniq.join('|')})", e]
+      }
+    end
+
+    def dynamic_predicates
+      @dynamic_predicates ||= generate_dynamic_predicates
+    end
+
+    def generate_dynamic_predicates
+      config[:dynamic_predicates].map {|e|
+        mtags = Tag.machine_tags(e); ["#{mtags[0].namespace}:*=(#{mtags.map(&:value).uniq.join('|')})", mtags[0].predicate]
+      }
+    end
+
     def machine_tag_reload
-      machine_tag_config(true)
-      generated_predicates(true)
-    end
-
-    def generated_predicates(reload=false)
-      global_predicates(reload) + wildcard_predicates(reload)
+      @config = read_config
+      @dynamic_predicates = generate_dynamic_predicates
+      @global_predicates = generate_global_predicates
     end
 
     # used to determine what default predicates are set depending on namespace and value
     def default_predicates
-      (machine_tag_config[:static_predicates] || [] + generated_predicates).map {|e| ["^"+e[0]+"$", e[1]]}
+      (config[:static_predicates] || [] + dynamic_predicates + global_predicates).
+      map {|e| ["^"+e[0]+"$", e[1]] }
     end
   end
 end
