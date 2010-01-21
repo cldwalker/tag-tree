6ee86070eac0b44c57b4f5a52286975e090da725
Gabriel Horner <gabriel.horner@gmail.com>
2010-01-21 17:15:54 -0500

    implicit namespacing for adding/removing mtags

--- lib/boson/commands/url_finder_commands.rb
+++ lib/boson/commands/url_finder_commands.rb
@@ -1,22 +1,39 @@
 module UrlFinderCommands
   def self.url_methods
-    %w{tag_add_and_remove tag_remove_and_save tag_add_and_save tag_list} +
-      %w{regex_update_attribute tag_and_save}
+     %w{tag_list regex_update_attribute tag_and_save}
+  end
+
+  def self.implicit_tag_methods
+    %w{tag_add_and_remove tag_remove_and_save tag_add_and_save}
   end
 
   def self.config
-    { :commands=>url_methods.inject({}) {|t,e|
-        t[e] = {:args=>'*', :option_command=>true}; t
+    { :commands=>(url_methods + implicit_tag_methods).inject({}) {|t,e|
+        t[e] = {:args=>[['uid'], ['*implicit_tags']], :option_command=>true}; t
       }
     }
   end
 
-  generated_methods = url_methods.map do |m|
-      %[
-        def #{m}(finder, *args)
-        ::Url.find(finder).#{m}(*args)
-        end
-      ]
+  generated_methods = url_methods.map do |meth|
+    %[
+      def #{meth}(uid, *args)
+        ::Url.find(uid).#{meth}(*args)
+      end
+    ]
+  end.join("\n") +
+  implicit_tag_methods.map do |meth|
+    %[
+      def #{meth}(uid, *mtags)
+        ourl = ::Url.find(uid)
+        mtags.map! {|e| implicit_tag_filter(e, ourl) }
+        ourl.#{meth}(*mtags)
+      end
+    ]
   end.join("\n")
   module_eval generated_methods
+
+  # Adds primary namespace from a url if a machine tag has no namespace
+  def implicit_tag_filter(mtag, ourl)
+    mtag[/#{Tag::PREDICATE_DELIMITER}/] ? mtag : ourl.tag_list.primary_namespace.to_s + Tag::PREDICATE_DELIMITER + mtag
+  end
 end
