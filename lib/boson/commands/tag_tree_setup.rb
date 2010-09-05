902b845af70dee81570544aa0e63c49cde8f0bd8
Gabriel Horner <gabriel.horner@gmail.com>
2010-09-05 13:52:01 -0400

    move config/alias loading elsewhere

--- lib/boson/commands/tag_tree_setup.rb
+++ lib/boson/commands/tag_tree_setup.rb
@@ -1,7 +1,6 @@
 module TagTreeSetup
   def self.after_included
     if Object.const_defined?(:IRB_PROCS)
-      IRB_PROCS[:tag_tree_aliases] = lambda {|e| Alias.create :file=>'config/alias.yml'}
       if $".include?('bond.rb')
         IRB_PROCS[:reload_bond] = lambda {|e| ::Readline.completion_proc = ::Bond.agent }
       end
