eeb03699173f01e4e9dcfd54153ba85c9c6e3c16
Gabriel Horner <gabriel.horner@gmail.com>
2011-03-13 17:27:58 -0400

    IRB_PROCS tweak

--- lib/boson/commands/tag_tree_setup.rb
+++ lib/boson/commands/tag_tree_setup.rb
@@ -2,7 +2,7 @@ module TagTreeSetup
   def self.after_included
     if Object.const_defined?(:IRB_PROCS)
       if $".include?('bond.rb')
-        IRB_PROCS[:reload_bond] = lambda {|e| ::Readline.completion_proc = ::Bond.agent }
+        IRB_PROCS[:reload_bond] = lambda { ::Readline.completion_proc = ::Bond.agent }
       end
     end
   end
